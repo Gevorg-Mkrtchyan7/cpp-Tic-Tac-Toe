357090aca460a20606b69c1e405d19784cad190e
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-07-01 02:03:06 +0400

    Game

--- main.cpp
+++ main.cpp
@@ -52,16 +52,11 @@ bool didEnd(char board[3][3]){
 	for(int i = 0; i < 3; ++i){
 		for(int j = 0; j < 3; ++j){
 			if(board[i][j] == empty){
-				++count;
-				break;
+				return false;
 			}
 		}
 	}
-	if(count == 0){
-		return true;
-	}else{
-		return false;
-	}
+	return true;	
 }
 
 int main(){
