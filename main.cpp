fbed989334ccc6cfb6527c640d1410cdfca31072
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-07-01 13:31:01 +0400

    Game

--- main.cpp
+++ main.cpp
@@ -17,25 +17,14 @@ bool legalMove(int r, int c, char board[3][3]){
 }
 
 bool didWin(char player, char board[3][3]){
-	if(board[0][0] == player && board[0][1] == player && board[0][2] == player) {
-		return true;
-	}  
-	if(board[1][0] == player && board[1][1] == player && board[1][2] == player) {
-		return true;
-	} 
-	if(board[2][0] == player && board[2][1] == player && board[2][2] == player) {
-		return true;
-	}
-	
-	if(board[0][0] == player && board[1][0] == player && board[2][0] == player) {
-		return true;
-	}
-	if(board[0][1] == player && board[1][1] == player && board[2][1] == player) {
-		return true;
-	}
-	if(board[0][2] == player && board[1][2] == player && board[2][2] == player) {
-		return true;
-	}
+	for(int i = 0; i < 3; ++i){
+		if(board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+			return true;
+		}  
+		if(board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+			return true;
+		}
+	}	
 
 	if(board[0][0] == player && board[1][1] == player && board[2][2] == player) {
 		return true;
